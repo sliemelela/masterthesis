cc05c690e1729b8365d1ff304ff15e6eb9d2fc18
sliemelela <sliemelela@gmail.com>
2023-03-01 12:59:16 +0100

    Added extra features to generation of datasets.
    Added the following:
    - Calculate zero-dimensional diagrams and adding that to the loss
    - Extra function that has periodic boundary conditions on the box

--- Programming/Inverse Optimization/src/inverse_optim/gen_data.py
+++ Programming/Inverse Optimization/src/inverse_optim/gen_data.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
 import torch
+import tadasets
 
 ### ALPHA FILTRATIONS ###
 
@@ -36,11 +37,18 @@ def create_alpha_pd(pts):
     p1b = torch.tensor([i[0] for i in p if len(i[0]) == 2])
     p1d = torch.tensor([i[1] for i in p if len(i[0]) == 2])
 
-    # Compute the distance between the extremities of the birth edge
-    if len(p1b) == 0 and len(p1d) == 0:
-        diag = torch.tensor([])
+    # Keep only pairs that contribute to H0, i.e. (vertex, edge), and separate birth (p1b0) and death (p1d0)
+    # Skipping the infinities by checking second part instead of first part.
+    p0b = torch.tensor([i[0] for i in p if len(i[1]) == 2])
+    p0d = torch.tensor([i[1] for i in p if len(i[1]) == 2])
+
+
+    # Compute the distance between the extremities of the birth edge for H1
+    if len(p1b) == 0:
+        diag1 = torch.tensor([])
     else:
         b = torch.norm(pts[p1b[:,1]] - pts[p1b[:,0]], dim=-1, keepdim=True)
+
         # For the death triangle, compute the maximum of the pairwise distances
         d_1 = torch.norm(pts[p1d[:,1]] - pts[p1d[:,0]], dim=-1, keepdim=True)
         d_2 = torch.norm(pts[p1d[:,1]] - pts[p1d[:,2]], dim=-1, keepdim=True)
@@ -48,9 +56,21 @@ def create_alpha_pd(pts):
         d = torch.max(d_1, torch.max(d_2, d_3))
 
         # *Not* the same as the finite part of st.persistence_intervals_in_dimension(1)
-        diag = torch.cat((b,d), 1)
+        diag1 = torch.cat((b,d), 1)
     
-    return diag
+    # Compute the distance between the extremities of the birth edge for H0
+    if len(p0b) == 0:
+        diag0 = torch.tensor([])
+    else:
+        # All birth times are 0 for the zero dimensional features
+        b0 = torch.tensor([[0] for _ in range(len(p0b))])
+
+        # Calculate the death times 
+        d0 = torch.norm(pts[p0d[:,1]] - pts[p0d[:,0]], dim=-1, keepdim=True)
+
+        diag0 = torch.cat((b0,d0), 1)
+    
+    return [diag0, diag1]
 
 def myloss_alpha(pts, goal_pd, sliced=False, thetas=torch.tensor([k/4 * np.pi for k in range(5)])):
     """
@@ -69,15 +89,21 @@ def myloss_alpha(pts, goal_pd, sliced=False, thetas=torch.tensor([k/4 * np.pi fo
     """
 
     # Creation of persistene diagram
-    diag = create_alpha_pd(pts)
+    diags = create_alpha_pd(pts)
+    diag0 = diags[0]
+    diag1 = diags[1]
 
+    goal_pd0 = goal_pd[0]
+    goal_pd1 = goal_pd[1]
     # Total persistence is a special case of Wasserstein distance
     if sliced == False:
-        dist = wasserstein_distance(diag, goal_pd, order=1, enable_autodiff=True, keep_essential_parts=False)  
+        dist0 = wasserstein_distance(diag0, goal_pd0, order=1, enable_autodiff=True, keep_essential_parts=False)  
+        dist1 = wasserstein_distance(diag1, goal_pd1, order=1, enable_autodiff=True, keep_essential_parts=False)  
+        dist = dist0 + dist1
         return dist
     else:
         # dist = sliced_wasserstein_distance([diag, goal_pd], thetas[3])
-        dists = [sliced_wasserstein_distance([diag, goal_pd], theta) for theta in thetas]
+        dists = [sliced_wasserstein_distance([diag1, goal_pd1], theta) for theta in thetas]
         dist = sum(dists)/len(dists)
         return dist
 
@@ -104,7 +130,6 @@ def generate_data_alpha(goal_pd, amount, dim, lr, epochs, decay_speed=10, invest
         - Plots of the generated dataset every 100 epochs including the final epoch 
     """
 
-    # Creation of initial random dataset
     pts = (torch.rand((amount, dim))).requires_grad_()
 
     # Set up optimizer for SGD
@@ -118,10 +143,11 @@ def generate_data_alpha(goal_pd, amount, dim, lr, epochs, decay_speed=10, invest
         min_mag_list = []
         avg_mag_list = []
     
-    # # Plot initial point cloud
-    # P = pts.detach().numpy()
-    # plt.scatter(P[:, 0], P[:, 1])
-    # plt.show()
+    if investigate == False:
+        # Plot initial point cloud
+        P = pts.detach().numpy()
+        plt.scatter(P[:, 0], P[:, 1])
+        plt.show()
 
     # Perform SGD
     for epoch in tqdm(range(epochs)):
@@ -209,7 +235,7 @@ def sliced_wasserstein_distance(dgms, theta):
 
 ### Periodicity tryout
 def per(x,y):
-    z = x % y
+    z = torch.fmod(x,y)
     return z.sum()
 
 def per_generate_data_alpha(goal_pd, amount, dim, lr, epochs, per1, per2, decay_speed=10, investigate=False, sliced=False, thetas=torch.tensor([k/4 * np.pi for k in range(5)])):
@@ -240,6 +266,7 @@ def per_generate_data_alpha(goal_pd, amount, dim, lr, epochs, per1, per2, decay_
     pts = pts * per1
     pts.requires_grad_()
 
+    box_size = torch.tensor([per1, per1])
     # Set up optimizer for SGD
     opt = torch.optim.SGD([pts], lr=lr)
     scheduler = LambdaLR(opt,[lambda epoch: decay_speed/(decay_speed+epoch)])
@@ -251,10 +278,10 @@ def per_generate_data_alpha(goal_pd, amount, dim, lr, epochs, per1, per2, decay_
         min_mag_list = []
         avg_mag_list = []
     
-    # Plot initial point cloud
-    P = pts.detach().numpy()
-    plt.scatter(P[:, 0], P[:, 1])
-    plt.show()
+    # # Plot initial point cloud
+    # P = pts.detach().numpy()
+    # plt.scatter(P[:, 0], P[:, 1])
+    # plt.show()
 
     # Perform SGD
     for epoch in tqdm(range(epochs)):
@@ -265,12 +292,33 @@ def per_generate_data_alpha(goal_pd, amount, dim, lr, epochs, per1, per2, decay_
         if investigate:
             loss_list.append(myloss_alpha(pts, goal_pd, sliced, thetas).item())
             
-        
-        opt.step()
+        # Update the coordinates of the points
+        with torch.no_grad():
+            # # Apply periodic boundary conditions
+            # pts_update = pts - opt.param_groups[0]['lr'] * pts.grad
+            # pts_update = (pts_update + box_size/2) % box_size - box_size/2
+
+            # # Update the parameters with the modified updates
+            # opt.step(pts_update - pts)
+
+            pts -= scheduler.get_last_lr()[0] * pts.grad
+
+            # Apply periodic boundary conditions
+            pts = pts % per1
+            # pts = torch.fmod(pts, per1)
+            # pts = torch.fmod((pts + box_size/2), box_size) - box_size/2
+
+            pts.requires_grad_()
+
+            # Zero out the gradients
+            # pts.grad.zero_()
+            
+        # opt.step()
         scheduler.step()
 
-        pts = pts % per1
-        print(pts)
+        # pts = pts % per1
+        
+        # print(pts.requires_grad)
 
         # if investigate:
         #     movement = pts.grad * scheduler.get_last_lr()[0]
